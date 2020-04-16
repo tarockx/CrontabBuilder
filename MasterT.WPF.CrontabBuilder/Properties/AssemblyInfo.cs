405d5d1f44d28cea6c79740a22d1fb6d91b05c78
Elia Dal Santo <elia.dal.santo@e-projectsrl.it>
2020-04-16 16:00:16 +0200

    Removed incorrectly pushed files

--- MasterT.WPF.CrontabBuilder/Properties/AssemblyInfo.cs
+++ MasterT.WPF.CrontabBuilder/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
 [assembly: Guid("626756d1-0a0b-49f5-b9af-52e194be459e")]
 
